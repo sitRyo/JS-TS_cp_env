fd5b637d633492b1830210663a4d7c93e4bf5e3f
sitRyo <al15030@shibaura-it.ac.jp>
2019-02-16 17:41:46 +0900

    initcommit

---
+++ src/components/EditorSpace.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="editor-space">
+    <div id="editor"></div>
+  </div>
+</template>
+
+<script>
+import ace from 'ace-builds'
+import 'ace-builds/src-noconflict/mode-typescript'
+import 'ace-builds/src-noconflict/mode-javascript'
+import 'ace-builds/src-noconflict/theme-monokai'
+
+export default {
+  props: ['isExecute'],
+  name: 'EditorSpace',
+  data() {
+    return {
+      contents: 
+      `function Main(input) {
+
+}
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+`,
+      editor: {},
+      editorId: 'editor'
+    }
+  },
+  mounted () {
+    this.editor = ace.edit( this.editorId )
+    this.editor.setValue(this.contents, 1)
+
+    this.editor.setTheme('ace/theme/monokai');
+    this.editor.getSession().setMode('ace/mode/javascript')
+  },
+  watch: {
+    'isExecute' () {
+      if (this.isExecute) {
+        this.contents = this.editor.getValue()
+        this.$emit('exe', this.contents)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#editor {
+  position: absolute;
+  width: 500px;
+  height: 500px;
+}
+</style>
+
---
+++ src/components/IO.vue
@@ -0,0 +1,84 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col>
+        <editor-space 
+          @exe="callbackExecuteButton"
+          :is-execute="isExecute">
+        </editor-space>
+      </b-col>
+      <b-col>
+        <button @click="pushExecuteButton">Execute</button>
+        <div class="io-wrapper">
+          <div class="input">
+            <div class="input-top">input</div>
+            <textarea v-model="input"></textarea>
+          </div>
+          <div class="output">
+            <div class="output-top">output</div>
+            <textarea :value="output"></textarea>
+          </div>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import EditorSpace from './EditorSpace.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  name: 'ioPos',
+  data () {
+    return {
+      input: '',
+      output: '',
+      isExecute: false,
+
+      editorContents: '',
+    }
+  },
+  methods: {
+    callbackExecuteButton: function (contents) {
+      this.output = ''
+      
+      console.log = (v) => {
+        this.output += String(v) + "\n"
+      }
+      this.isExecute = false
+      const exec = this.runCode(this.formedCode(contents))
+      exec()
+    },
+
+    pushExecuteButton: function () {
+      this.isExecute = true
+    },
+
+    runCode: function (obj) {
+      return new Function(obj)
+    },
+
+    formedCode: function (contents) {
+      const inputDeclaration = 'var args = `' + this.input + '`;\n'
+      const executeOrder = 'Main(args);\n'
+      const insertOrder = inputDeclaration + executeOrder
+
+      const inputOrder = 'Main(require("fs").readFileSync("/dev/stdin", "utf8"))'
+      const inputOrderStart = contents.indexOf(inputOrder)
+
+      const formedOrder = contents.substr(0,inputOrderStart-1) + '\n' + insertOrder
+
+      return formedOrder
+    }
+  },
+  components: {
+    EditorSpace,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
